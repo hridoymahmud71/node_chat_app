0d7069043747c34946544e9e7df4f8184989f426
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-01-05 15:18:10 +0600

    check login middleware working

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -5,9 +5,11 @@ const router = express.Router();
 // internal imports
 const { getInbox } = require("./../controller/inboxController");
 const decoratedHtmlResponse = require("./../middlewares/common/decoratedHtmlResponse");
+const checkLogin = require("./../middlewares/common/checkLogin");
+
 
 
 // login page
-router.get("/",decoratedHtmlResponse("Inbox"), getInbox);
+router.get("/",decoratedHtmlResponse("Inbox"),checkLogin, getInbox);
 
 module.exports = router;
--- router/userRouter.js
+++ router/userRouter.js
@@ -5,15 +5,17 @@ const router = express.Router();
 // internal imports
 const { getUsers,addUser,deleteUser } = require("./../controller/userController");
 const decoratedHtmlResponse = require("./../middlewares/common/decoratedHtmlResponse");
+const checkLogin = require("./../middlewares/common/checkLogin");
 const avatarUpload = require("../middlewares/users/avatarUpload");
 const { addUserValidator,addUserValidatorHandler } = require("../middlewares/users/userValidators");
 
 // login page
-router.get("/", decoratedHtmlResponse("Users"), getUsers);
+router.get("/", decoratedHtmlResponse("Users"),checkLogin, getUsers);
 
 // add user
 router.post(
   "/",
+  checkLogin,
   avatarUpload,
   addUserValidator,
   addUserValidatorHandler,
