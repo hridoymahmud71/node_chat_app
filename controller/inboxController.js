4e3ce0387524379d7e4aea54e6bc745e09a0fd45
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-01-06 10:55:24 +0600

    conversation ongoing

--- controller/inboxController.js
+++ controller/inboxController.js
@@ -1,5 +1,12 @@
 
-// get login page
+// external imports
+const createError = require("http-errors");
+
+// internal imports
+const User = require("../models/People");
+const Conversation = require("../models/Conversation");
+
+// get inbox page
 function getInbox(req,res,next) {
    
     res.render("inbox");    
@@ -10,7 +17,12 @@ async function search(req,res,next) {
     
 }
 
+async function addConversation(req,res,next) {
+    
+}
+
 module.exports = {
     getInbox,
-    search
+    search,
+    addConversation
 }
--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 // internal imports
-const { getInbox,search } = require("./../controller/inboxController");
+const { getInbox,search,addConversation } = require("./../controller/inboxController");
 const decoratedHtmlResponse = require("./../middlewares/common/decoratedHtmlResponse");
 const checkLogin = require("./../middlewares/common/checkLogin");
 
@@ -11,6 +11,11 @@ const checkLogin = require("./../middlewares/common/checkLogin");
 
 // login page
 router.get("/",decoratedHtmlResponse("Inbox"),checkLogin, getInbox);
+
+// search conversation
 router.post("/search",checkLogin, search);
+
+// add a conversation
+router.post("/conversation",checkLogin, addConversation);
 
 module.exports = router;
